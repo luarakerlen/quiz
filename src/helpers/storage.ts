64617801acc748d30e179cd49a8300b0df878765
Luara Kerlen dos Santos Rangel <26902816+luarakerlen@users.noreply.github.com>
2022-11-05 00:26:38 -0300

    create get data hook

--- src/helpers/storage.ts
+++ src/helpers/storage.ts
@@ -1,4 +1,5 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useEffect, useState } from 'react';
 import { Alert } from 'react-native';
 
 interface storedQuestionDataProps {
@@ -30,4 +31,33 @@ export async function saveAnswer(
 		console.log('Erro ao salvar resposta: ', e);
 		Alert.alert('Erro ao salvar!', 'Não foi possível registrar sua resposta.');
 	}
+}
+
+export async function clearAnswers() {
+	try {
+		await AsyncStorage.clear();
+	} catch (e) {
+		console.log('Erro ao excluir dados: ', e);
+		Alert.alert('Erro!', 'Não foi possível continuar.');
+	}
+}
+
+export function useStoredQuestionsData() {
+	const [storedQuestionsData, setStoredQuestionsData] = useState([]);
+	async function getAnswers() {
+		try {
+			const data = await AsyncStorage.getItem(dataKey);
+			const currentData = data ? JSON.parse(data) : [];
+			setStoredQuestionsData(currentData);
+		} catch (e) {
+			console.log('Erro ao recuperar dados do storage: ', e);
+			Alert.alert('Erro!', 'Não foi possível continuar.');
+		}
+	}
+
+	useEffect(() => {
+		getAnswers();
+	}, []);
+
+	return { storedQuestionsData };
 }
