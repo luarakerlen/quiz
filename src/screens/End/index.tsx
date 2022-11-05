87805076e784ec38d79b8fec84ddd287d5c283cd
Luara Kerlen dos Santos Rangel <26902816+luarakerlen@users.noreply.github.com>
2022-11-05 00:42:32 -0300

    clear questions

--- src/screens/End/index.tsx
+++ src/screens/End/index.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
+import { useNavigation } from '@react-navigation/native';
 import { DefaultButton, DefaultContainer } from '../../components';
-
+import { clearAnswers } from '../../helpers';
+import { useStoredQuestionsData } from '../../hooks';
 import { Card, IconsContainer, Title, Subtitle } from './styles';
 
 export function End() {
+	const { storedQuestionsData } = useStoredQuestionsData();
+	const navigation = useNavigation<any>();
+	// console.log('respostas: ', storedQuestionsData);
+	// console.log('respostas: ', storedQuestionsData.length);
+
 	const subtitle = {
 		success: `Congratulations!\nYou passed`,
 		fail: 'To pass the test, you need get at least 7 answers right',
 	};
+
+	function handleTryAgain() {
+		clearAnswers();
+		navigation.pop(2);
+	}
 
 	return (
 		<DefaultContainer>
@@ -15,7 +27,7 @@ export function End() {
 				<IconsContainer></IconsContainer>
 				<Title>You've reached 6 out of 10</Title>
 				<Subtitle>{subtitle.success}</Subtitle>
-				<DefaultButton text='Try again' onPress={() => {}} />
+				<DefaultButton text='Try again' onPress={handleTryAgain} />
 			</Card>
 		</DefaultContainer>
 	);
