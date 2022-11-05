348e00a4f39f22bfa6e12e3102c5ffb29d56ce70
Luara Kerlen dos Santos Rangel <26902816+luarakerlen@users.noreply.github.com>
2022-11-04 22:59:54 -0300

    create save question data helper

---
+++ src/helpers/storage.ts
@@ -0,0 +1,34 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Alert } from 'react-native';
+
+interface storedQuestionDataProps {
+	questionNumber: number;
+	correctAnswer: string;
+	givenAnswer: string;
+}
+
+const dataKey = '@quiz:questions';
+
+export async function saveAnswer(
+	questionNumber: number,
+	correctAnswer: string,
+	givenAnswer: string
+) {
+	const newStoredQuestionData: storedQuestionDataProps = {
+		questionNumber,
+		correctAnswer,
+		givenAnswer,
+	};
+
+	try {
+		const data = await AsyncStorage.getItem(dataKey);
+		const currentData = data ? JSON.parse(data) : [];
+		const updatedData = [...currentData, newStoredQuestionData];
+
+		await AsyncStorage.setItem(dataKey, JSON.stringify(updatedData));
+		console.log('dados salvos: ', updatedData);
+	} catch (e) {
+		console.log('Erro ao salvar resposta: ', e);
+		Alert.alert('Erro ao salvar!', 'Não foi possível registrar sua resposta.');
+	}
+}
